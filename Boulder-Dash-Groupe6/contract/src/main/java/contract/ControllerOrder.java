5ee2a525028af46ba04d2fb046ea5e8b3607ea0b
Queenbee-ui <58205376+Queenbee-ui@users.noreply.github.com>
2020-05-26 12:25:15 +0100

    Updated classes

---
+++ Boulder-Dash-Groupe6/contract/src/main/java/contract/ControllerOrder.java
@@ -0,0 +1,37 @@
+package contract;
+
+
+/**
+ * The ControllerOrder enumeration
+ * @author Touna Alexis
+ * @version 1.0
+ *
+ */
+public enum ControllerOrder {
+
+	/** The first map */
+	Map1,
+	/** The second map */
+	Map2,
+	/** The third map */
+	Map3,
+	/** The fourth map */
+	Map4,
+	/** The fifth map */
+	Map5,
+	/** The sixth map */
+	Map6,
+	/** The seventh map */
+	Map7,
+	/** To go up */
+	Z,
+	/** To go left */
+	Q,
+	/** To go down */
+	S,
+	/** To go right */
+	D,
+	/** To do nothing */
+	nothing
+	
+}
